5cb63b70f252b78475c4d4090d0787e68b0b5406
nxc8912 <notcolyeah@gmail.com>
2025-09-23 20:08:18 -0500

    Update proj0.c

--- proj0.c
+++ proj0.c
@@ -6,7 +6,7 @@ int
 main(int argc, char *argv[])
 {
   int i;
-  printf("CSE3320 proj0 printing in user space:");
+  printf(1, "CSE3320 proj0 printing in user space:");
   for(i = 1; i < argc; i++)
     printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
   exit();
